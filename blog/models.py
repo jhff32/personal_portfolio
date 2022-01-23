1e55603895a49d83869f14d4f10b954e5bc77277
jhff32 <66910676+jhff32@users.noreply.github.com>
2022-01-24 01:42:19 +0200

    added program "blog"

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+
+class Project(models.Model):
+    title = models.CharField(max_length=200)
+    date = models.DateField(auto_now_add=True)
+    description = models.TextField(max_length=250)
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from .models import Project
 
 
 def all_blogs(request):
-    return render(request, 'blog/all_blogs.html')
+    projects = Project.objects.order_by('-date')[:5]
+    return render(request, 'blog/all_blogs.html', {'projects': projects})
