92cb36a90177ad7ce608c46a5591bee2de74bfdf
jhff32 <66910676+jhff32@users.noreply.github.com>
2022-01-28 00:54:12 +0200

    added links to blog posts

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Project
 
 
 def all_blogs(request):
     projects = Project.objects.order_by('-date')[:5]
     return render(request, 'blog/all_blogs.html', {'projects': projects})
+
+
+def detail(request, blog_id):
+    blog = get_object_or_404(Project, pk=blog_id)
+    return render(request, 'blog/detail.html', {'blog': blog})
