8b324e5d6a4e89a9edbf0a7274844e599d72adfd
jhff32 <66910676+jhff32@users.noreply.github.com>
2022-02-06 00:31:59 +0200

    added css styles and base.html

--- blog/models.py
+++ blog/models.py
@@ -5,3 +5,6 @@ class Project(models.Model):
     title = models.CharField(max_length=200)
     date = models.DateField(auto_now_add=True)
     description = models.TextField(max_length=250)
+
+    def __str__(self):
+        return self.title
--- blog/views.py
+++ blog/views.py
@@ -3,7 +3,7 @@ from .models import Project
 
 
 def all_blogs(request):
-    projects = Project.objects.order_by('-date')[:5]
+    projects = Project.objects.order_by('-date')
     return render(request, 'blog/all_blogs.html', {'projects': projects})
 
 
